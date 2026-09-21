8d8a309047939fd99007ed58193ec5d8f6d64a37
niuboyi0609-dot <niuboyi0609@gmail.com>
2026-09-21 16:47:40 +0800

    Update prompt generator design and portfolio collage

---
+++ portfolio-collage.js
@@ -0,0 +1,726 @@
+/* Hallmark · portfolio collage · generated 2026-09-21 */
+(() => {
+  const RAIL = [
+  {
+    "name": "hanging-hummingbird",
+    "side": "left",
+    "top": 0.85,
+    "left": -21.41,
+    "width": 49.93,
+    "rotate": -19.64,
+    "z": 1,
+    "opacity": 0.97,
+    "duration": 13.27,
+    "delay": 1.13,
+    "aspect": 1.0426
+  },
+  {
+    "name": "bookend-dragon-blue-1",
+    "side": "left",
+    "top": -1.85,
+    "left": 25.13,
+    "width": 69.85,
+    "rotate": -7.39,
+    "z": 2,
+    "opacity": 0.94,
+    "duration": 14.39,
+    "delay": 5.21,
+    "aspect": 1.0723
+  },
+  {
+    "name": "tissue-unicorn",
+    "side": "left",
+    "top": 7.84,
+    "left": -8.48,
+    "width": 75.03,
+    "rotate": -0.87,
+    "z": 3,
+    "opacity": 0.94,
+    "duration": 12.71,
+    "delay": 5.82,
+    "aspect": 1.1305
+  },
+  {
+    "name": "hanging-athena-gold",
+    "side": "left",
+    "top": 10.38,
+    "left": 33.25,
+    "width": 85.11,
+    "rotate": 2.0,
+    "z": 4,
+    "opacity": 0.99,
+    "duration": 14.57,
+    "delay": 0.34,
+    "aspect": 0.589
+  },
+  {
+    "name": "hanging-cat-black",
+    "side": "left",
+    "top": 19.84,
+    "left": -12.66,
+    "width": 57.91,
+    "rotate": 12.56,
+    "z": 5,
+    "opacity": 0.95,
+    "duration": 12.1,
+    "delay": 4.42,
+    "aspect": 0.7889
+  },
+  {
+    "name": "bookend-dragon-red-1",
+    "side": "left",
+    "top": 22.45,
+    "left": 34.76,
+    "width": 65.36,
+    "rotate": -14.83,
+    "z": 6,
+    "opacity": 0.94,
+    "duration": 12.16,
+    "delay": 5.22,
+    "aspect": 1.2544
+  },
+  {
+    "name": "tissue-sunflower",
+    "side": "left",
+    "top": 33.68,
+    "left": -14.9,
+    "width": 77.77,
+    "rotate": -4.54,
+    "z": 7,
+    "opacity": 0.96,
+    "duration": 14.69,
+    "delay": 1.72,
+    "aspect": 1.2152
+  },
+  {
+    "name": "hanging-moon-fairy",
+    "side": "left",
+    "top": 31.04,
+    "left": 26.14,
+    "width": 81.22,
+    "rotate": -0.94,
+    "z": 8,
+    "opacity": 0.9,
+    "duration": 13.72,
+    "delay": 4.7,
+    "aspect": 0.8444
+  },
+  {
+    "name": "hanging-duck-green",
+    "side": "left",
+    "top": 42.46,
+    "left": -6.65,
+    "width": 50.63,
+    "rotate": 10.62,
+    "z": 9,
+    "opacity": 0.93,
+    "duration": 11.95,
+    "delay": 2.3,
+    "aspect": 0.9529
+  },
+  {
+    "name": "tissue-blue-floral",
+    "side": "left",
+    "top": 42.62,
+    "left": 28.26,
+    "width": 60.82,
+    "rotate": 10.16,
+    "z": 10,
+    "opacity": 0.92,
+    "duration": 16.99,
+    "delay": 0.06,
+    "aspect": 1.0
+  },
+  {
+    "name": "hanging-bluebird-heart",
+    "side": "left",
+    "top": 56.2,
+    "left": -9.14,
+    "width": 75.97,
+    "rotate": -10.89,
+    "z": 11,
+    "opacity": 0.93,
+    "duration": 15.32,
+    "delay": 1.93,
+    "aspect": 0.9489
+  },
+  {
+    "name": "hanging-rabbit-color",
+    "side": "left",
+    "top": 54.77,
+    "left": 37.19,
+    "width": 85.47,
+    "rotate": -3.93,
+    "z": 12,
+    "opacity": 0.98,
+    "duration": 12.24,
+    "delay": 5.56,
+    "aspect": 0.5784
+  },
+  {
+    "name": "hanging-cat-green",
+    "side": "left",
+    "top": 65.93,
+    "left": -12.74,
+    "width": 54.81,
+    "rotate": 3.33,
+    "z": 13,
+    "opacity": 0.98,
+    "duration": 16.12,
+    "delay": 1.71,
+    "aspect": 0.9725
+  },
+  {
+    "name": "tissue-lotus",
+    "side": "left",
+    "top": 64.66,
+    "left": 25.03,
+    "width": 70.64,
+    "rotate": 10.19,
+    "z": 14,
+    "opacity": 0.91,
+    "duration": 13.65,
+    "delay": 5.89,
+    "aspect": 1.2991
+  },
+  {
+    "name": "hanging-athena-2",
+    "side": "left",
+    "top": 76.32,
+    "left": -6.89,
+    "width": 80.41,
+    "rotate": 15.83,
+    "z": 15,
+    "opacity": 0.93,
+    "duration": 16.07,
+    "delay": 3.52,
+    "aspect": 0.921
+  },
+  {
+    "name": "hanging-fox-warm",
+    "side": "left",
+    "top": 75.17,
+    "left": 34.77,
+    "width": 87.56,
+    "rotate": -7.18,
+    "z": 16,
+    "opacity": 0.98,
+    "duration": 15.39,
+    "delay": 5.58,
+    "aspect": 1.152
+  },
+  {
+    "name": "hanging-cat-moon",
+    "side": "left",
+    "top": 87.29,
+    "left": -17.71,
+    "width": 56.74,
+    "rotate": -0.19,
+    "z": 17,
+    "opacity": 0.94,
+    "duration": 12.85,
+    "delay": 3.6,
+    "aspect": 1.0248
+  },
+  {
+    "name": "tissue-autumn-2",
+    "side": "left",
+    "top": 88.35,
+    "left": 34.99,
+    "width": 67.93,
+    "rotate": 0.21,
+    "z": 18,
+    "opacity": 0.94,
+    "duration": 11.39,
+    "delay": 4.2,
+    "aspect": 1.1375
+  },
+  {
+    "name": "hanging-parrot-wall",
+    "side": "right",
+    "top": 0.36,
+    "left": -18.84,
+    "width": 52.25,
+    "rotate": -16.48,
+    "z": 1,
+    "opacity": 0.96,
+    "duration": 12.11,
+    "delay": 4.12,
+    "aspect": 0.8936
+  },
+  {
+    "name": "bookend-dragon-blue-2",
+    "side": "right",
+    "top": -0.19,
+    "left": 38.38,
+    "width": 63.11,
+    "rotate": -9.57,
+    "z": 2,
+    "opacity": 0.99,
+    "duration": 14.32,
+    "delay": 4.05,
+    "aspect": 1.5859
+  },
+  {
+    "name": "tissue-santa",
+    "side": "right",
+    "top": 10.55,
+    "left": -15.53,
+    "width": 79.26,
+    "rotate": -1.95,
+    "z": 3,
+    "opacity": 0.99,
+    "duration": 15.71,
+    "delay": 3.48,
+    "aspect": 1.0143
+  },
+  {
+    "name": "hanging-eagle-red",
+    "side": "right",
+    "top": 11.22,
+    "left": 39.63,
+    "width": 80.44,
+    "rotate": 4.37,
+    "z": 4,
+    "opacity": 0.97,
+    "duration": 13.73,
+    "delay": 4.49,
+    "aspect": 0.7417
+  },
+  {
+    "name": "hanging-cat-heart",
+    "side": "right",
+    "top": 22.88,
+    "left": -7.52,
+    "width": 55.34,
+    "rotate": 12.28,
+    "z": 5,
+    "opacity": 0.95,
+    "duration": 15.11,
+    "delay": 0.71,
+    "aspect": 1.1041
+  },
+  {
+    "name": "bookend-dragon-red-2",
+    "side": "right",
+    "top": 23.83,
+    "left": 25.07,
+    "width": 69.78,
+    "rotate": -12.67,
+    "z": 6,
+    "opacity": 0.9,
+    "duration": 16.83,
+    "delay": 4.74,
+    "aspect": 1.5584
+  },
+  {
+    "name": "tissue-autumn",
+    "side": "right",
+    "top": 33.65,
+    "left": -9.94,
+    "width": 74.91,
+    "rotate": -6.15,
+    "z": 7,
+    "opacity": 0.97,
+    "duration": 11.95,
+    "delay": 5.64,
+    "aspect": 1.0112
+  },
+  {
+    "name": "hanging-triangle-sail",
+    "side": "right",
+    "top": 30.89,
+    "left": 29.45,
+    "width": 79.46,
+    "rotate": -2.57,
+    "z": 8,
+    "opacity": 0.97,
+    "duration": 12.89,
+    "delay": 2.3,
+    "aspect": 0.8742
+  },
+  {
+    "name": "hanging-bunny-window",
+    "side": "right",
+    "top": 46.25,
+    "left": -15.59,
+    "width": 56.26,
+    "rotate": 6.66,
+    "z": 9,
+    "opacity": 0.94,
+    "duration": 13.78,
+    "delay": 3.36,
+    "aspect": 0.5722
+  },
+  {
+    "name": "tissue-poppy",
+    "side": "right",
+    "top": 42.71,
+    "left": 33.5,
+    "width": 62.2,
+    "rotate": 14.71,
+    "z": 10,
+    "opacity": 1.0,
+    "duration": 11.41,
+    "delay": 3.42,
+    "aspect": 1.0633
+  },
+  {
+    "name": "hanging-santa",
+    "side": "right",
+    "top": 53.39,
+    "left": -8.67,
+    "width": 73.44,
+    "rotate": -8.98,
+    "z": 11,
+    "opacity": 0.95,
+    "duration": 14.44,
+    "delay": 3.96,
+    "aspect": 1.1036
+  },
+  {
+    "name": "hanging-fox-blue",
+    "side": "right",
+    "top": 55.46,
+    "left": 35.18,
+    "width": 85.82,
+    "rotate": -1.02,
+    "z": 12,
+    "opacity": 0.98,
+    "duration": 16.8,
+    "delay": 1.32,
+    "aspect": 0.6736
+  },
+  {
+    "name": "hanging-magician",
+    "side": "right",
+    "top": 65.74,
+    "left": -8.89,
+    "width": 58.33,
+    "rotate": -0.96,
+    "z": 13,
+    "opacity": 0.94,
+    "duration": 12.26,
+    "delay": 2.19,
+    "aspect": 0.9786
+  },
+  {
+    "name": "tissue-irises",
+    "side": "right",
+    "top": 64.44,
+    "left": 36.11,
+    "width": 67.49,
+    "rotate": 12.01,
+    "z": 14,
+    "opacity": 0.99,
+    "duration": 14.46,
+    "delay": 5.02,
+    "aspect": 0.9762
+  },
+  {
+    "name": "hanging-fox-purple",
+    "side": "right",
+    "top": 75.63,
+    "left": -21.46,
+    "width": 70.07,
+    "rotate": 19.84,
+    "z": 15,
+    "opacity": 0.97,
+    "duration": 13.48,
+    "delay": 5.41,
+    "aspect": 0.859
+  },
+  {
+    "name": "hanging-heart-tulip",
+    "side": "right",
+    "top": 78.44,
+    "left": 24.91,
+    "width": 87.95,
+    "rotate": -11.99,
+    "z": 16,
+    "opacity": 0.99,
+    "duration": 13.84,
+    "delay": 2.52,
+    "aspect": 1.0038
+  },
+  {
+    "name": "hanging-bunny-window-2",
+    "side": "right",
+    "top": 88.65,
+    "left": -8.21,
+    "width": 51.52,
+    "rotate": -0.49,
+    "z": 17,
+    "opacity": 0.91,
+    "duration": 13.24,
+    "delay": 0.73,
+    "aspect": 0.6
+  },
+  {
+    "name": "bookend-dragon-blue-3",
+    "side": "right",
+    "top": 88.73,
+    "left": 28.97,
+    "width": 60.3,
+    "rotate": 7.75,
+    "z": 18,
+    "opacity": 0.97,
+    "duration": 16.68,
+    "delay": 4.54,
+    "aspect": 1.2
+  }
+];
+  const STRIP = [
+  {
+    "name": "hanging-hummingbird",
+    "width": 106.99,
+    "rotate": -11.68,
+    "overlap": -8.0,
+    "z": 1,
+    "opacity": 0.95,
+    "duration": 15.46,
+    "delay": 4.25
+  },
+  {
+    "name": "bookend-dragon-blue-1",
+    "width": 129.46,
+    "rotate": 3.71,
+    "overlap": -14.0,
+    "z": 2,
+    "opacity": 0.96,
+    "duration": 14.22,
+    "delay": 4.4
+  },
+  {
+    "name": "tissue-unicorn",
+    "width": 157.93,
+    "rotate": -6.73,
+    "overlap": -20.0,
+    "z": 3,
+    "opacity": 0.92,
+    "duration": 17.15,
+    "delay": 0.88
+  },
+  {
+    "name": "hanging-athena-gold",
+    "width": 167.97,
+    "rotate": 3.79,
+    "overlap": -8.0,
+    "z": 4,
+    "opacity": 0.92,
+    "duration": 15.37,
+    "delay": 4.03
+  },
+  {
+    "name": "hanging-cat-black",
+    "width": 100.67,
+    "rotate": -9.02,
+    "overlap": -14.0,
+    "z": 5,
+    "opacity": 0.92,
+    "duration": 12.13,
+    "delay": 3.96
+  },
+  {
+    "name": "bookend-dragon-red-1",
+    "width": 137.15,
+    "rotate": 0.57,
+    "overlap": -20.0,
+    "z": 6,
+    "opacity": 0.9,
+    "duration": 13.58,
+    "delay": 1.89
+  },
+  {
+    "name": "tissue-sunflower",
+    "width": 155.3,
+    "rotate": -4.58,
+    "overlap": -8.0,
+    "z": 7,
+    "opacity": 0.98,
+    "duration": 15.47,
+    "delay": 1.0
+  },
+  {
+    "name": "hanging-moon-fairy",
+    "width": 178.55,
+    "rotate": 4.26,
+    "overlap": -14.0,
+    "z": 8,
+    "opacity": 0.94,
+    "duration": 14.97,
+    "delay": 4.52
+  },
+  {
+    "name": "hanging-duck-green",
+    "width": 110.41,
+    "rotate": -5.0,
+    "overlap": -20.0,
+    "z": 9,
+    "opacity": 0.91,
+    "duration": 13.3,
+    "delay": 4.85
+  },
+  {
+    "name": "tissue-blue-floral",
+    "width": 136.28,
+    "rotate": 7.05,
+    "overlap": -8.0,
+    "z": 10,
+    "opacity": 0.95,
+    "duration": 17.98,
+    "delay": 2.11
+  },
+  {
+    "name": "hanging-bluebird-heart",
+    "width": 145.33,
+    "rotate": -7.1,
+    "overlap": -14.0,
+    "z": 11,
+    "opacity": 0.92,
+    "duration": 14.44,
+    "delay": 4.05
+  },
+  {
+    "name": "hanging-rabbit-color",
+    "width": 181.41,
+    "rotate": 4.5,
+    "overlap": -20.0,
+    "z": 12,
+    "opacity": 0.94,
+    "duration": 16.28,
+    "delay": 0.17
+  },
+  {
+    "name": "hanging-cat-green",
+    "width": 113.59,
+    "rotate": -6.82,
+    "overlap": -8.0,
+    "z": 13,
+    "opacity": 0.99,
+    "duration": 16.74,
+    "delay": 1.47
+  },
+  {
+    "name": "tissue-lotus",
+    "width": 130.74,
+    "rotate": 4.42,
+    "overlap": -14.0,
+    "z": 14,
+    "opacity": 0.93,
+    "duration": 13.52,
+    "delay": 3.14
+  },
+  {
+    "name": "hanging-parrot-wall",
+    "width": 147.47,
+    "rotate": -5.54,
+    "overlap": -20.0,
+    "z": 15,
+    "opacity": 0.95,
+    "duration": 17.51,
+    "delay": 2.07
+  },
+  {
+    "name": "hanging-cat-heart",
+    "width": 175.44,
+    "rotate": 6.7,
+    "overlap": -8.0,
+    "z": 16,
+    "opacity": 0.91,
+    "duration": 15.14,
+    "delay": 0.84
+  },
+  {
+    "name": "hanging-athena-2",
+    "width": 102.75,
+    "rotate": -1.27,
+    "overlap": -14.0,
+    "z": 17,
+    "opacity": 1.0,
+    "duration": 14.87,
+    "delay": 3.44
+  },
+  {
+    "name": "hanging-fox-warm",
+    "width": 129.79,
+    "rotate": 8.6,
+    "overlap": -20.0,
+    "z": 18,
+    "opacity": 0.95,
+    "duration": 14.91,
+    "delay": 4.63
+  },
+  {
+    "name": "hanging-fox-purple",
+    "width": 148.69,
+    "rotate": 0.86,
+    "overlap": -8.0,
+    "z": 19,
+    "opacity": 1.0,
+    "duration": 14.53,
+    "delay": 2.74
+  },
+  {
+    "name": "hanging-cat-moon",
+    "width": 178.51,
+    "rotate": 8.19,
+    "overlap": -14.0,
+    "z": 20,
+    "opacity": 0.97,
+    "duration": 12.48,
+    "delay": 4.23
+  },
+  {
+    "name": "hanging-heart-tulip",
+    "width": 102.84,
+    "rotate": 0.96,
+    "overlap": -20.0,
+    "z": 21,
+    "opacity": 0.93,
+    "duration": 14.78,
+    "delay": 4.03
+  },
+  {
+    "name": "bookend-dragon-blue-3",
+    "width": 124.57,
+    "rotate": -12.52,
+    "overlap": -8.0,
+    "z": 22,
+    "opacity": 0.96,
+    "duration": 14.32,
+    "delay": 1.31
+  }
+];
+  const railLeft = document.querySelector('#portfolioRailLeft');
+  const railRight = document.querySelector('#portfolioRailRight');
+  const stripTrack = document.querySelector('#portfolioStripTrack');
+  if (!railLeft || !railRight || !stripTrack) return;
+  const create = (item, mode) => {
+    const image = document.createElement('img');
+    image.className = `portfolio-cut portfolio-cut--${mode}`;
+    image.src = `./portfolio-cut/${item.name}.webp`;
+    image.alt = '';
+    image.loading = 'eager';
+    image.decoding = 'async';
+    image.style.setProperty('--w', mode === 'rail' ? `${item.width}%` : `${item.width}px`);
+    if (mode === 'rail') {
+      image.style.setProperty('--x', `${item.left}%`);
+      image.style.setProperty('--y', `${item.top}%`);
+    } else {
+      image.style.setProperty('--overlap', `${item.overlap}px`);
+    }
+    image.style.setProperty('--rot', `${item.rotate}deg`);
+    image.style.setProperty('--z', item.z);
+    image.style.setProperty('--opacity', item.opacity);
+    image.style.setProperty('--dur', `${item.duration}s`);
+    image.style.setProperty('--delay', `${item.delay}s`);
+    return image;
+  };
+  for (const item of RAIL) {
+    (item.side === 'left' ? railLeft : railRight).appendChild(create(item, 'rail'));
+  }
+  for (const item of STRIP) {
+    stripTrack.appendChild(create(item, 'strip'));
+  }
+})();
